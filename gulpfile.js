73865422b3e572d0b376e79e62d8cbb2560bb985
Ярослав <spartacus215@yandex.ru>
2021-05-21 12:31:14 +0300

    Финальные исправления. Теперь точно все готово.

--- gulpfile.js
+++ gulpfile.js
@@ -98,6 +98,7 @@ exports.sprite = sprite;
 const copy = (done) => {
   gulp.src([
     "source/fonts/*.{woff2,woff}",
+    "source/manifest.webmanifest",
     "source/*.ico",
   ], {
     base: "source"
